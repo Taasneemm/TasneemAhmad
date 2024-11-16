cad2d130a141e7b75b7f6a1f0ac3e7b288b81df3
Tasneem Ahmad <tasneemahmd96@gmail.com>
2024-11-16 18:54:05 +0800

    updated changes

--- assignment3/question5.js
+++ assignment3/question5.js
@@ -1,3 +1,30 @@
 const bacData = [190.15, 194.21, 191.07, 192.44, 129000];
 
 // Write your code below
+/* 
+- index 0 - Low
+- index 1 - High
+- index 2 - Open
+- index 3 - Close
+- index 4 - Volume
+*/
+
+if (bacData[3] > bacData[2]) {
+    if (bacData[4] > 100000) {
+        console.log("Strong bullish");
+    } else {
+        console.log("Bullish");
+    }
+} else if (bacData[3] < bacData[2]) {
+    if (bacData[4] > 10000) {
+        console.log("Strong Bearish");
+    } else {
+        console.log("Bearish");
+    }
+} else {
+    console.log("neutral");
+}
+
+
+    
+
